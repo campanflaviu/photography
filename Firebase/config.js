1c34a88b811a3fecf82eee0a7040c8f98bd469e7
Cimpan Flaviu <fcimpan@macadamian.com>
2023-02-23 16:48:59 +0200

    removed firebase credentials and moved them to env variables

--- Firebase/config.js
+++ Firebase/config.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA5aGVV9B7FxzCqm01z-LwUg9vdNJlUSTA",
-  authDomain: "photography-8f8b5.firebaseapp.com",
-  projectId: "photography-8f8b5",
-  storageBucket: "photography-8f8b5.appspot.com",
-  messagingSenderId: "876529191945",
-  appId: "1:876529191945:web:355b0d81abfdb9fb6c7cd8"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
