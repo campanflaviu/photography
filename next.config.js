792049a16160e02a19380fd551a27c1aaae1d333
Cimpan Flaviu <fcimpan@macadamian.com>
2023-02-23 16:51:52 +0200

    commented carousel package

--- next.config.js
+++ next.config.js
@@ -16,6 +16,11 @@ const nextConfig = {
         hostname: 'firebasestorage.googleapis.com',
         port: '',
         pathname: '/**',
+      }, {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
       },
     ]
   },
